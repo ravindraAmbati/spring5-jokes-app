ba6be7b7f3100764096f4f873ee9cb09b7b9389c
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-18 11:00:06 +0530

    add a service layer, closes #6

---
+++ src/main/java/com/springbootapps/spring5jokesapp/Services/JokeService.java
@@ -0,0 +1,6 @@
+package com.springbootapps.spring5jokesapp.Services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/springbootapps/spring5jokesapp/Services/JokeServiceImpl.java
@@ -0,0 +1,17 @@
+package com.springbootapps.spring5jokesapp.Services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
