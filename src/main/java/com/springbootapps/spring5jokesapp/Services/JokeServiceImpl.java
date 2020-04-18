1259b024360571ae49a0825e31897da2ae41c8e3
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-04-18 19:01:50 +0530

    refactor: create a xml config for ChuckNorrisQuotes, closes #12

--- src/main/java/com/springbootapps/spring5jokesapp/Services/JokeServiceImpl.java
+++ src/main/java/com/springbootapps/spring5jokesapp/Services/JokeServiceImpl.java
@@ -1,9 +1,11 @@
 package com.springbootapps.spring5jokesapp.Services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Service;
 
 @Service
+@ImportResource("classpath:chuck-config.xml")
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
